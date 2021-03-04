bf9247faa86f81be302e9e336e6ebfc4b4d81fa3
Gerard <Bradshaw>
2021-03-03 16:54:53 -0800

    Added WeatherView module
    Sourced from https://github.com/MatteoBattilana/WeatherView

---
+++ weatherview/src/main/java/com/github/matteobattilana/weather/WeatherData.kt
@@ -0,0 +1,11 @@
+package com.github.matteobattilana.weather
+
+/**
+ * Created by Mitchell on 7/6/2017.
+ */
+interface WeatherData {
+    val precipType: PrecipType
+    val emissionRate: Float
+    val speed: Int
+    val scaleFactor: Float
+}
---
+++ weatherview/src/main/java/com/github/matteobattilana/weather/confetti/WeatherConfettoGenerator.kt
@@ -0,0 +1,89 @@
+package com.github.matteobattilana.weather.confetti
+
+import android.graphics.*
+import com.github.jinatonic.confetti.ConfettoGenerator
+import com.github.jinatonic.confetti.confetto.Confetto
+import com.github.matteobattilana.weather.PrecipType
+import java.util.*
+
+/**
+ * Created by Mitchell on 7/4/2017.
+ */
+class WeatherConfettoGenerator(val confettoInfo: ConfettoInfo) : ConfettoGenerator {
+  override fun generateConfetto(random: Random): Confetto = MotionBlurBitmapConfetto(confettoInfo)
+}
+
+class MotionBlurBitmapConfetto(val confettoInfo: ConfettoInfo) : Confetto() {
+  companion object {
+    const val SNOW_RADIUS = 7.5f
+    const val RAIN_STRETCH = 20f
+  }
+
+  private val transparentWhite by lazy {
+    Color.argb(0, Color.red(Color.WHITE), Color.green(Color.WHITE), Color.blue(Color.WHITE))
+  }
+
+  override fun getHeight(): Int = 0
+
+  override fun getWidth(): Int = 0
+
+  override fun configurePaint(paint: Paint) {
+    super.configurePaint(paint)
+    paint.color = Color.WHITE
+    paint.isAntiAlias = true
+  }
+
+  override fun drawInternal(
+    canvas: Canvas,
+    matrix: Matrix,
+    paint: Paint,
+    x: Float,
+    y: Float,
+    rotation: Float,
+    percentageAnimated: Float
+  ) {
+    when (confettoInfo.precipType) {
+      PrecipType.CLEAR -> { /* Do nothing */ }
+      PrecipType.RAIN,
+      PrecipType.DRIZZLE,
+      PrecipType.LIGHT_RAIN,
+      PrecipType.HEAVY_RAIN,
+      PrecipType.EXTREME_RAIN,
+      PrecipType.VERY_HEAVY_RAIN -> {
+        val rainStretch = RAIN_STRETCH * (confettoInfo.scaleFactor + 1.0f) / 2f;
+        val dX = currentVelocityX
+        val dY = currentVelocityY
+        val x1 = x - dX * rainStretch
+        val y1 = y - dY * rainStretch
+        val x2 = x + dX * rainStretch
+        val y2 = y + dY * rainStretch
+
+        paint.strokeWidth = confettoInfo.scaleFactor;
+        paint.shader = LinearGradient(x1, y1, x2, y2,
+          intArrayOf(Color.TRANSPARENT, Color.WHITE, Color.WHITE, Color.TRANSPARENT),
+          floatArrayOf(0f, 0.45f, 0.55f, 1f),
+          Shader.TileMode.CLAMP)
+
+        canvas.drawLine(x1, y1, x2, y2, paint)
+      }
+      PrecipType.SNOW -> {
+        val sigmoid = (1f / (1f + Math.pow(Math.E, -(confettoInfo.scaleFactor.toDouble() - 1f)))).toFloat()
+        paint.shader = RadialGradient(x, y, SNOW_RADIUS * confettoInfo.scaleFactor,
+          intArrayOf(Color.WHITE, Color.WHITE, transparentWhite, transparentWhite),
+          floatArrayOf(0f, 0.15f + sigmoid * 0.30f, 0.95f - sigmoid * 0.35f, 1f),
+          Shader.TileMode.CLAMP)
+
+        canvas.drawCircle(x, y, SNOW_RADIUS * confettoInfo.scaleFactor, paint)
+      }
+      PrecipType.CUSTOM -> {
+        matrix.preTranslate(x, y)
+        matrix.preRotate(rotation,
+          confettoInfo.customBitmap!!.width / 2f,
+          confettoInfo.customBitmap!!.height / 2f
+        );
+        matrix.preScale(confettoInfo.scaleFactor, confettoInfo.scaleFactor)
+        canvas.drawBitmap(confettoInfo.customBitmap!!, matrix, paint)
+      }
+    }
+  }
+}
